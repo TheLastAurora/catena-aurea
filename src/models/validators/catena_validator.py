ee8621e683ac8c43286868b5a9b0686c10fa10b3
Vitor L <vitoribeiro0024@gmail.com>
2023-11-23 11:38:42 -0300

    Brainwriting on validation workflow

--- src/models/validators/catena_validator.py
+++ src/models/validators/catena_validator.py
@@ -23,17 +23,45 @@ global data
 with open(os.path.join(os.path.dirname(__file__), "../schemas/catena_schema.json")) as f:
     schema = json.load(f)
 
-def _request_data():
-    """Gets the data for the given pattern"""
+
+def _by_evangelista(evangelium: str) -> list[dict]:
+    """Returns all the chapters from evangelium"""
+    # THIS IS THE STARTPOINT 1
     pass
- 
-def _filter_data():
-    """Checks whether the data exists or not, filtering based on the type of search
-    
-        evangelist -> returns all chapters;
-        evangelist, chapter -> returns all verses from the chapter;
-        evangelist, chapter, verse -> returns all comments for the given verse;
-        evangelist, commentator -> returns all comments of any evangelist given the commentator;
-        commentator -> returns all comments from this commentator.
-    """
+
+def _by_commentator(exegeta: str) -> list[dict]:
+    """Returns all comments made by the exegeta"""
+    # THIS IS THE STARTPOINT 2
     pass
+
+def _by_chapter(gospel: list) -> list[dict]:
+    """Filters all gospels per chapter"""
+    pass
+
+def _by_verse(chapter: list) -> list[dict] | dict:
+    """Filters all chapters from gospel per verse (or verses)"""
+    pass
+
+
+def extract_data(*args, **kwargs) -> dict:
+    """Establishes an interface for extracting the data"""
+
+    # User input, thats why is in latin, not english.
+
+    _args = frozenset([kwarg for kwarg in kwargs.keys() if kwarg])
+    # Defining execution flow (filtering)
+    func_mapping = {
+        frozenset(['exegeta']): _by_commentator(),
+        frozenset(['evangelista']): _by_evangelista(),
+        frozenset(['evangelista', 'exegeta']): _by_evangelista(_by_commentator()),
+        frozenset(['evangelista', 'capitulum']): _by_chapter(_by_evangelista()),
+        frozenset(['evangelista', 'capitulum', 'versus']): _by_verse(_by_chapter(_by_evangelista())),
+        frozenset(['evangelista', 'capitulum', 'versus', 'exegeta']): _by_verse(_by_chapter(_by_evangelista(_by_commentator())))
+    }
+
+    # It probably doesn't work yet, this is just to remember the concept.
+    res = func_mapping[_args](args, **kwargs)
+    return res
+
+
+
